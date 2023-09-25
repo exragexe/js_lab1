3a229146b55fb91cd9e00e7e33f467d26dcc82ba
exragexe <78488184+exragexe@users.noreply.github.com>
2023-09-25 20:20:29 +0300

    Update main.js

--- lab1/main.js
+++ lab1/main.js
@@ -2,9 +2,9 @@ let num_a = parseInt(prompt("Enter num a: "));
 let num_b = parseInt(prompt("Enter num b: "));
 let formula_for_sum_numbers ={
     priority1:Math.pow(num_a+num_b,2),
-    priority2:Math.pow(num_a,2)+2*num_a*num_b,
+    priority2:(Math.pow(num_a,2)+((2*num_a)*num_b)),
     priority3: num_b === 0 ? alert("Error! Number b can't be 0!") : Math.pow(num_b, 2)
 };
-alert(formula_for_sum_numbers.priority1-formula_for_sum_numbers.priority2
+alert((formula_for_sum_numbers.priority1 - formula_for_sum_numbers.priority2)
     / formula_for_sum_numbers.priority3);
 
